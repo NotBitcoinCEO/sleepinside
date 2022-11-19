9075019c364f71e959000105af77e07c85729c66
AleBravoP <bra21043@byui.edu>
2022-11-18 21:26:25 -0600

    Team Activity 5

---
+++ src/js/productData.js
@@ -0,0 +1,24 @@
+const baseURL = 'http://server-nodejs.cit.byui.edu:3000/'
+
+function convertToJson(res) {
+    if (res.ok) {
+      return res.json();
+    } else {
+      throw new Error('Bad Response');
+    }
+  }
+  
+  export default class ProductData  {
+    constructor() {
+      // this.category = category;
+      // this.path = `../json/${this.category}.json`;
+    }
+    getData(category) {
+      return fetch(baseURL + `products/search/${category}`)
+        .then(convertToJson).then((data) => data.Result);
+    }
+    async findProductById(id) {
+      const products = await this.getData()
+      return products.find((item) => item.Id === id);
+    }
+  }
